b0bdd0302869465c01140c6bdfdee59daf5f9ba7
eagle-shop <22486435+eagle-shop@users.noreply.github.com>
2019-10-23 00:18:54 +0900

    Add static link to call glfw API

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include "mywindow.h"
+#include "glfw3.h"
 
 int main(){
     std::cout << "main" << std::endl;
     sub();
+
+    if(!glfwInit()){
+        return (-1);
+    }
+
     return 0;
 }
